eaedc27afa7c5e7f9ac58ea6780d4c68c22855d4
Gregor Santner <gsantner@mailbox.org>
2017-05-19 15:05:57 +0200

    v0.2.9

--- screenshots/createLayout.sh
+++ screenshots/createLayout.sh
@@ -7,13 +7,24 @@ if [ ! -d "$1" ] ; then
 	mkdir -p "$1/en/phoneScreenshots"
 	mkdir -p "$1/en/sevenInchScreenshots"
 	mkdir -p "$1/en/tenInchScreenshots"
-else
+fi
+if [ -d "$1" ] ; then
 	[ ! -f "$1/de/icon.png" ] && ln -s "../../../graphics/app_icon/app_icon_512.png" "$1/de/icon.png"
 	[ ! -f "$1/en/icon.png" ] && ln -s "../../../graphics/app_icon/app_icon_512.png" "$1/en/icon.png"
 	[ ! -f "$1/de/featureGraphic.png" ] && ln -s "../../../graphics/app_icon/featureGraphic.png" "$1/de/featureGraphic.png"
 	[ ! -f "$1/en/featureGraphic.png" ] && ln -s "../../../graphics/app_icon/featureGraphic.png" "$1/en/featureGraphic.png"
 	[ ! -f "$1/de/promoGraphic.png" ] && ln -s "../../../graphics/app_icon/promoGraphic.png" "$1/de/promoGraphic.png"
 	[ ! -f "$1/en/promoGraphic.png" ] && ln -s "../../../graphics/app_icon/promoGraphic.png" "$1/en/promoGraphic.png"
+
+    if [ -d "$2" ] ; then
+        UP="\.\.\/"
+        for i in "$2/de/phoneScreenshots/"* ; do [ -f "$i" ] && ln -s "$(echo $i | sed "s/$2/$UP$UP$UP$2/")" "$(echo $i | sed "s/$2/$1/")" ;  done;
+        for i in "$2/en/phoneScreenshots/"* ; do [ -f "$i" ] && ln -s "$(echo $i | sed "s/$2/$UP$UP$UP$2/")" "$(echo $i | sed "s/$2/$1/")" ;  done;
+        for i in "$2/de/sevenInchScreenshots/"* ; do [ -f "$i" ] && ln -s "$(echo $i | sed "s/$2/$UP$UP$UP$2/")" "$(echo $i | sed "s/$2/$1/")" ;  done;
+        for i in "$2/en/sevenInchScreenshots/"* ; do [ -f "$i" ] && ln -s "$(echo $i | sed "s/$2/$UP$UP$UP$2/")" "$(echo $i | sed "s/$2/$1/")" ;  done;
+        for i in "$2/de/tenInchScreenshots/"* ; do [ -f "$i" ] && ln -s "$(echo $i | sed "s/$2/$UP$UP$UP$2/")" "$(echo $i | sed "s/$2/$1/")" ;  done;
+        for i in "$2/en/tenInchScreenshots/"* ; do [ -f "$i" ] && ln -s "$(echo $i | sed "s/$2/$UP$UP$UP$2/")" "$(echo $i | sed "s/$2/$1/")" ;  done;
+    fi
 
 	#[ -f "$1/en/promoGraphic.png" -a ! -f "$1/de/promoGraphic.png" ] && ln -s "../en/promoGraphic.png" "$1/de/promoGraphic.png"
 fi
